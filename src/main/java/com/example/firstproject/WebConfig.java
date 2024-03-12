70f83c54311715bbe0d8c8af2431c61b57205691
seomyeong-gwan <myeonggwanseo70@naver.com>
2024-03-12 22:19:43 +0900

    웹 애플리케이션의 설정 정보를 담을 WebConfig 생성 - LoginCheckInterceptor 등록

---
+++ src/main/java/com/example/firstproject/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.firstproject;
+
+import com.example.firstproject.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 로그인 체크 인터셉터 등록
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/board/write");
+    }
+}

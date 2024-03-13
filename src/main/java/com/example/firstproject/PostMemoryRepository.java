d6b71b2995d755a6609935a0722f37b25369198c
seomyeong-gwan <myeonggwanseo70@naver.com>
2024-03-13 21:38:05 +0900

    스프링이 기본으로 제공하는 포맷터 @DateTimeFormat 적용으로 인한 코드 수정

--- src/main/java/com/example/firstproject/PostMemoryRepository.java
+++ src/main/java/com/example/firstproject/PostMemoryRepository.java
@@ -3,18 +3,17 @@ package com.example.firstproject;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Repository
 public class PostMemoryRepository implements PostRepository {
-    private static Map<Long, Post> postStore = new HashMap<>();
+    private static final Map<Long, Post> postStore = new HashMap<>();
     private static Long sequence = 0L;
 
     @Override
     public void save(Post post) {
         post.setBno(++sequence);
-        post.setRegisterTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")));
+        post.setRegisterTime(LocalDateTime.now());
 
         postStore.put(post.getBno(), post);
     }
@@ -25,8 +24,7 @@ public class PostMemoryRepository implements PostRepository {
 
         findPost.setTitle(updatePost.getTitle());
         findPost.setContent(updatePost.getContent());
-        findPost.setRegisterTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yy-MM-dd HH:mm")));
-
+        findPost.setRegisterTime(LocalDateTime.now());
     }
 
     @Override

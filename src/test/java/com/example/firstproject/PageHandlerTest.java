d85b39c0ddb3daa7800ea4b7ae41da8d0fd3d48c
seomyeong-gwan <myeonggwanseo70@naver.com>
2024-03-15 22:55:16 +0900

    PageHandler 테스트 코드 작성

---
+++ src/test/java/com/example/firstproject/PageHandlerTest.java
@@ -0,0 +1,31 @@
+package com.example.firstproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PageHandlerTest {
+    @Test
+    public void pagingTest1() {
+        PageHandler ph = new PageHandler(250, 1);
+        assertThat(ph.getBeginPage()).isEqualTo(1);
+        assertThat(ph.getEndPage()).isEqualTo(10);
+        ph.print();
+    }
+
+    @Test
+    public void pagingTest2() {
+        PageHandler ph = new PageHandler(250, 15);
+        assertThat(ph.getBeginPage()).isEqualTo(11);
+        assertThat(ph.getEndPage()).isEqualTo(20);
+        ph.print();
+    }
+
+    @Test
+    public void pagingTest3() {
+        PageHandler ph = new PageHandler(255, 23);
+        assertThat(ph.getBeginPage()).isEqualTo(21);
+        assertThat(ph.getEndPage()).isEqualTo(26);
+        ph.print();
+    }
+}

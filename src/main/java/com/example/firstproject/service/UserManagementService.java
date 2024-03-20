72181bd5929598c568e8d294074754a88da74bc7
seomyeong-gwan <myeonggwanseo70@naver.com>
2024-03-20 23:44:21 +0900

    디렉토리 구조 변경(service 패키지 생성)으로 인한 UserManagementService 생성 - 회원 관리에 관한 비즈니스 로직 수행

---
+++ src/main/java/com/example/firstproject/service/UserManagementService.java
@@ -0,0 +1,36 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.domain.Member;
+import com.example.firstproject.repository.MemberRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserManagementService {
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원을 추가하는 메서드, 즉 회원가입 기능을 하는 메서드
+     */
+    public void addMember(Member member) {
+        memberRepository.add(member);
+    }
+
+    /**
+     * 회원을 삭제하는 메서드, 즉 회원탈퇴 기능을 하는 메서드
+     */
+    public void deleteMember(Member member) {
+        memberRepository.deleteByNo(member.getNo());
+    }
+
+    /**
+     * 회원탈퇴 시, 회원탈퇴 전에 로그인으로 인해서 생성된 세션을 종료시키는 메서드
+     */
+    public void terminateSession(HttpServletRequest request) {
+        request.getSession().invalidate();
+    }
+}
